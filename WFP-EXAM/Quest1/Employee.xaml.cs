bb40d6b71c8ad64406a35c9605793195b5912f63
Vũ Việt Anh <73330605+vabi1232@users.noreply.github.com>
2021-11-23 02:25:04 +0700

    Add project files.

---
+++ WFP-EXAM/Quest1/Employee.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace WFP_EXAM.Quest1
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class Employee : Page
+    {
+        public Employee()
+        {
+            this.InitializeComponent();
+        }
+
+        private List<HidDeviceClass> HidDeviceClasses;
+
+        public class HidDeviceClass
+        {
+            public HidDeviceClass(string usageName, ushort pageID, ushort usageID)
+            {
+                UsageName = usageName;
+                PageID = pageID;
+                UsageID = usageID;
+            }
+
+            public string UsageName { get; set; }
+            public ushort PageID { get; set; }
+            public ushort UsageID { get; set; }
+        }
+        private void GetUagePageInfo()
+        {
+            using (StreamReader file = File.OpenText(".\\usagepage.json"))
+            using (JsonTextReader reader = new JsonTextReader(file))
+            {
+                JObject jObject = (JObject)JToken.ReadFrom(reader);
+                var hidDeviceClasses =
+                    from p in jObject["hidDeviceClasses"]
+                    select new HidDeviceClass((string)p["UsageName"], (ushort)p["PageID"], (ushort)p["UsageID"]);
+                HidDeviceClasses = hidDeviceClasses.ToList<HidDeviceClass>();
+                cbDevType.DataContext = HidDeviceClasses;
+            }
+
+        }
+
+    }
+
+}
---
+++ WFP-EXAM/Quest2/Login.xaml.cs
@@ -0,0 +1,69 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using WFP_EXAM.Quest2.Utils;
+using System.Diagnostics;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace WFP_EXAM.Quest2
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class Login : Page
+    {
+        private Account _account;
+        public Login()
+        {
+            this.InitializeComponent();
+        }
+
+        protected override async void OnNavigatedTo(NavigationEventArgs e)
+        {
+            // Check Microsoft Passport is setup and available on this machine
+            if (await MicrosoftPassportHelper.MicrosoftPassportAvailableCheckAsync())
+            {
+            }
+            else
+            {
+                // Microsoft Passport is not setup so inform the user
+                PassportStatus.Background = new SolidColorBrush(Windows.UI.Color.FromArgb(255, 50, 170, 207));
+                PassportStatusText.Text = "Microsoft Passport is not setup!\nPlease go to Windows Settings and set up a PIN to use it.";
+                PassportSignInButton.IsEnabled = false;
+            }
+        }
+
+        private void PassportSignInButton_Click(object sender, RoutedEventArgs e)
+        {
+            ErrorMessage.Text = "";
+            SignInPassport();
+        }
+
+        private void RegisterButtonTextBlock_OnPointerPressed(object sender, PointerRoutedEventArgs e)
+        {
+            ErrorMessage.Text = "";
+        }
+
+        private async void SignInPassport()
+        {
+            if (AccountHelper.ValidateAccountCredentials(UsernameTextBox.Text))
+            {
+                // Create and add a new local account
+                _account = AccountHelper.AddAccount(UsernameTextBox.Text);
+                Debug.WriteLine("Successfully signed in with traditional credentials and created local account instance!");
+
+                if (await MicrosoftPassportHelper.CreatePassportKeyAsync(UsernameTextBox.Text))
+                {
+                    Debug.WriteLine("Successfully signed in with Microsoft Passport!");
+                }
+            }
+            else
+            {
+                ErrorMessage.Text = "Invalid Credentials";
+            }
+        }
+    }
+}
